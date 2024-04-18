c4a07afd0af610fc787d5c6ee2ebb1ac337264d9
julian fong <julianF1725@gmail.com>
2024-04-18 07:00:44 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -22,3 +26,52 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+let shortIndex = 0;
+const urls = new Map();
+const shortUrls = new Map();
+
+app.post('/api/shorturl', (req, res) => {
+  console.log(urls);
+  console.log(shortUrls);
+  const url = req.body.url;
+  const hostname = url.replace(/^https?:\/\//, '').replace(/\/$/, '');
+
+  dns.lookup(hostname, (error, address, family) => {
+    if (error) {
+      res.send({
+        "error": "invalid url"
+      });
+    } else {
+      let url_index = 0;
+      if (urls.has(url)) {
+        url_index = urls.get(url);
+      } else {
+        shortIndex++;
+        url_index = shortIndex;
+        urls.set(url, url_index);
+        shortUrls.set(url_index, url);
+      }
+
+      res.send({
+        "original_url": url,
+        "short_url": url_index
+      })
+    }
+  })
+
+  
+})
+
+app.use('/api/shorturl/:short_url?', (req, res) => {
+  const url = shortUrls.get(Number.parseInt(req.params.short_url));
+  console.log(url);
+  if (!url) {
+    res.send({
+      "error": "invalid short url"
+    });
+    return
+  }
+  res.redirect(url);
+})
